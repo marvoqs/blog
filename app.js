9092b2c41f8c46cb464223be4f85264a8664881d
Marek Schwarz <marvoqs@gmail.com>
2020-09-22 12:46:16 +0200

    install mongoose and add db connection

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+mongoose.connect(
+  `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.t7utl.mongodb.net/blog?retryWrites=true&w=majority`,
+  { useNewUrlParser: true }
+);
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 app.use(express.json({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('home');
 });
 
 app.listen(port, () => {
